dba608592adc6a9c969dc2c371967221ed2b0824
Joby Elliott <code@byjoby.com>
2022-05-24 09:57:26 -0600

    updates

--- scripts/cron-run.php
+++ scripts/cron-run.php
@@ -2,8 +2,15 @@
 
 use DigraphCMS\Cron\Cron;
 
-require_once __DIR__ . '/../vendor/autoload.php';
-require_once __DIR__ . '/../initialize.php';
+// get site name
+$siteName = basename(realpath(__DIR__ . '/../..'));
+$backupDir = '/home/handbook/public_html/_backup';
 
-set_time_limit(300);
-Cron::runJobs(time() + 60);
+try {
+    require_once __DIR__ . '/../vendor/autoload.php';
+    require_once __DIR__ . '/../initialize.php';
+    set_time_limit(300);
+    Cron::runJobs(time() + 60);
+} catch (\Throwable $th) {
+    echo "$siteName: cron: " . $th->getMessage();
+}
