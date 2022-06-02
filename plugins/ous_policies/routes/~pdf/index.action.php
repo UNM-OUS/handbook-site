dbddfe39dd5e45997943575b1445175efdfdb4c3
Joby Elliott <elhober@unm.edu>
2022-06-02 09:53:05 -0600

    upstream fixes

---
+++ plugins/ous_policies/routes/~pdf/index.action.php
@@ -0,0 +1,12 @@
+<h1>PDF download of Faculty Handbook</h1>
+<p>
+    Use these links to download PDF versions of either the entire Handbook or of individual sections.
+    The following PDFs are rebuilt once a day to ensure they always match the content of the website.
+    Downloaded copies will indicate the date they were built in the page footer.
+</p>
+<?php
+
+use DigraphCMS\UI\Breadcrumb;
+use DigraphCMS\URL\URL;
+
+Breadcrumb::parent(new URL('/policies/'));
