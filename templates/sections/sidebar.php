57b1e36fab79008f79b52315d39c5229016e3ebc
Joby Elliott <elhober@unm.edu>
2022-06-15 11:34:10 -0600

    more descriptive sidebar links

--- templates/sections/sidebar.php
+++ templates/sections/sidebar.php
@@ -10,12 +10,12 @@ $menu->addClass('menubar--vertical');
 $menu->addClass('menubar--manual-toggle');
 $menu->addClass('sidebar__menu');
 
-if ($page = Pages::get('section_a')) $menu->addPageDropdown($page, null, true);
-if ($page = Pages::get('section_b')) $menu->addPageDropdown($page, null, true);
-if ($page = Pages::get('section_c')) $menu->addPageDropdown($page, null, true);
-if ($page = Pages::get('section_d')) $menu->addPageDropdown($page, null, true);
-if ($page = Pages::get('section_e')) $menu->addPageDropdown($page, null, true);
-if ($page = Pages::get('section_f')) $menu->addPageDropdown($page, null, true);
+if ($page = Pages::get('section_a')) $menu->addPageDropdown($page, "Section A: The University", true);
+if ($page = Pages::get('section_b')) $menu->addPageDropdown($page, "Section B: Academic Freedom &amp; Tenure", true);
+if ($page = Pages::get('section_c')) $menu->addPageDropdown($page, "Section C: Faculty Rules and Benefits", true);
+if ($page = Pages::get('section_d')) $menu->addPageDropdown($page, "Section D: Teaching and Student-Related", true);
+if ($page = Pages::get('section_e')) $menu->addPageDropdown($page, "Section E: Research", true);
+if ($page = Pages::get('section_f')) $menu->addPageDropdown($page, "Section F: Branch Community Colleges", true);
 
 $menu->addURL(new URL('/pdf/'), 'Faculty Handbook PDFs');
 $menu->addURL(new URL('/abolished/'), 'Abolished policies');
