e15cd1c452e17ccac45479810e9cb627d1733835
Joby Elliott <elhober@unm.edu>
2022-06-21 13:34:10 -0600

    cron-run needs a context for when called from the command line

--- scripts/cron-run.php
+++ scripts/cron-run.php
@@ -1,6 +1,9 @@
 <?php
 
+use DigraphCMS\Context;
 use DigraphCMS\Cron\Cron;
+use DigraphCMS\URL\URL;
+use DigraphCMS\URL\URLs;
 
 // get site name
 $siteName = basename(realpath(__DIR__ . '/../..'));
@@ -9,8 +12,12 @@ $backupDir = '/home/handbook/public_html/_backup';
 try {
     require_once __DIR__ . '/../vendor/autoload.php';
     require_once __DIR__ . '/../initialize.php';
+    URLs::beginContext(new URL('/'));
+    Context::begin();
+    Context::url(new URL('/'));
     set_time_limit(300);
     Cron::runJobs(time() + 60);
+    Context::end();
 } catch (\Throwable $th) {
     echo "$siteName: cron: " . $th->getMessage();
 }
