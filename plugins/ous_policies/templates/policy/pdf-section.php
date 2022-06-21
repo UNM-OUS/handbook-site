7a28c0ff96dd6a4f78a92da7d64a197816b7b825
Joby Elliott <elhober@unm.edu>
2022-06-21 12:45:46 -0600

    try without table of contents page numbers

--- plugins/ous_policies/templates/policy/pdf-section.php
+++ plugins/ous_policies/templates/policy/pdf-section.php
@@ -15,6 +15,9 @@ use DigraphCMS\Media\Media;
         <?php echo Context::fields()['page.name'] ?? 'Untitled'; ?>
         :: <?php echo Context::fields()['site.name']; ?>
     </title>
+    <style>
+        <?php echo Media::get('/styles_pdf/*.css')->content(); ?>
+    </style>
 </head>
 
 <body>
@@ -23,17 +26,6 @@ use DigraphCMS\Media\Media;
         $GLOBALS['max_objects'] = 0;
     </script>
     <?php echo Context::fields()['body']; ?>
-    <script type="text/php">
-        for ($i = 0; $i <= $GLOBALS['max_objects']; $i++) {
-            if (!array_key_exists($i, $pdf->get_cpdf()->objects)) continue;
-            $object = $pdf->get_cpdf()->objects[$i];
-            if (!array_key_exists('c', $object)) continue;
-            foreach ($GLOBALS['toc'] as $id => $page) {
-                $object['c'] = str_replace( '%%'.$id.'%%' , $page , $object['c'] );
-            }
-            $pdf->get_cpdf()->objects[$i] = $object;
-        }
-    </script>
 </body>
 
 </html>
