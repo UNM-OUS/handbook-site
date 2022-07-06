864b9a447a20b2ee17e70ca91e100ef77c8f6d7f
Joby Elliott <elhober@unm.edu>
2022-07-06 10:59:48 -0600

    composer update, small fix

--- templates/sections/navbar.php
+++ templates/sections/navbar.php
@@ -10,6 +10,7 @@ if ($home = Pages::get('home')) $menu->addPage($home, 'Home');
 $menu->addURL(new URL('/policies/'), 'Policies');
 $menu->addURL(new URL('/policy_updates/'), 'Updates');
 $menu->addURL(new URL('/under_review/'), 'Under review');
-$menu->addURL(new URL('/resources/'), 'Info/Resources');
+$menu->addURL(new URL('/information/'), 'Information');
+$menu->addURL(new URL('/resources/'), 'Resources');
 $menu->addURL(new URL('/pdf/'), 'PDFs');
 echo $menu;
